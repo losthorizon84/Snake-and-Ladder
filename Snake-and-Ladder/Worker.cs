a5758a9a7f9a17fceac3ec4c5e5fd6ebfbc9f5a6
Oscar Campos <oscar@campos.es>
2022-01-12 10:24:35 +0100

    Control en la selección de usuario

--- Snake-and-Ladder/Worker.cs
+++ Snake-and-Ladder/Worker.cs
@@ -16,40 +16,51 @@ namespace Snake_and_Ladder
         {
             _gameService.CreateBoard();
 
-            Console.WriteLine();
-            Console.WriteLine("Seleccione el número de jugadores:");
-            Console.WriteLine("1. Juego de 1 Jugador");
-            Console.WriteLine("2. Juego de 2 Jugadores");
-            Console.WriteLine("3. Juego de 3 Jugadores");
-            Console.WriteLine("4. Juego de 4 Jugadores");
-            int nplayers = 0;
-            switch (Console.ReadLine())
+            bool validSelection = false;
+            while (!validSelection)
             {
-                case "1":
-                    nplayers = 1;
-                    break;
-                case "2":
-                    nplayers = 2;
-                    break;
-                case "3":
-                    nplayers = 3;
-                    break;
-                case "4":
-                    nplayers = 4;
-                    break;
-                default:
-                    Console.WriteLine("Opción no válida");
-                    break;
+                validSelection = true;
+                Console.WriteLine();
+                Console.WriteLine("Seleccione el número de jugadores:");
+                Console.WriteLine("1. Juego de 1 Jugador");
+                Console.WriteLine("2. Juego de 2 Jugadores");
+                Console.WriteLine("3. Juego de 3 Jugadores");
+                Console.WriteLine("4. Juego de 4 Jugadores");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Play(1);
+                        break;
+                    case "2":
+                        Play(2);
+                        break;
+                    case "3":
+                        Play(3);
+                        break;
+                    case "4":
+                        Play(4);
+                        break;
+                    default:
+                        validSelection = false;
+                        Console.Clear();
+                        Console.WriteLine("Opción no válida");
+                        break;
+                }
             }
+        }
 
-            for (int i = 0; i < nplayers; i++)
+        private void Play(int nPlayers)
+        {
+            for (int i = 0; i < nPlayers; i++)
             {
-                Console.WriteLine($"Indique el nombre del jugador {i+1}");
+                Console.WriteLine($"Indique el nombre del jugador {i + 1}");
                 string name = Console.ReadLine();
                 _gameService.AddPlayer(name, i + 1);
             }
 
             _gameService.Start();
         }
+
+
     }
 }
